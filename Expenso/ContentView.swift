7dfcac499381271b2aa1bf53cb51c76eb5fb0cee
Kapil Shanbhag <kapil.shanbhag@redbus.com>
2022-09-22 20:20:20 +0530

    Initial Commit

---
+++ Expenso/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Expenso
+//
+//  Created by Kapil Ganesh Shanbhag on 22/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Expenso/ExpensoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensoApp.swift
+//  Expenso
+//
+//  Created by Kapil Ganesh Shanbhag on 22/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpensoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
